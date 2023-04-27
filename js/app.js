c422f058efa90a16a6cbc5cad11dbd3d50404366
Izzatilla <108586467+01Izzat@users.noreply.github.com>
2023-04-27 15:39:33 +0500

    type

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const zBox = document.querySelector('.list')
+      zSelect = document.querySelector('#select')
 
 pokemons.forEach((element, index, array) => {
   let newLi = document.createElement("li")
@@ -7,7 +8,6 @@ pokemons.forEach((element, index, array) => {
       newDesc = document.createElement("p")
       newWeight = document.createElement("p")
 
-        
   newImg.setAttribute('src', element.img)
   newHeading.textContent = element.name
   newDesc.textContent = element.type
@@ -21,5 +21,53 @@ pokemons.forEach((element, index, array) => {
   newLi.appendChild(newDesc)
   newLi.appendChild(newWeight)
   zBox.appendChild(newLi)
-
 })
+
+
+let filterBox = []
+
+pokemons.forEach(value => {
+  value.type.forEach(item => {
+    if(!filterBox.includes(item)) {
+      filterBox.push(item)
+    }
+  })
+})
+
+filterBox.forEach(item => {
+  let zOption = document.createElement('option')
+  
+  zOption.setAttribute('value', item)
+  zOption.textContent = item
+
+  zSelect.appendChild(zOption)
+})
+
+  const changeSelect = (e) => {
+    zBox.innerHTML = null;
+    pokemons.map((value) => {
+      if(value.type.includes(e.target.value)) {
+        let newLi = document.createElement("li")
+      newImg = document.createElement("img")
+      newHeading = document.createElement("h2")
+      newDesc = document.createElement("p")
+      newWeight = document.createElement("p")
+
+      newImg.setAttribute('src', value.img)
+      newHeading.textContent = value.name
+      newDesc.textContent = value.type
+      newWeight.textContent = value.weight
+
+
+      newLi.style.cssText = 'border:1px solid; padding:20px'
+      
+      newLi.appendChild(newImg);
+      newLi.appendChild(newHeading)
+      newLi.appendChild(newDesc)
+      newLi.appendChild(newWeight)
+      zBox.appendChild(newLi)
+      }
+    })
+  }
+
+  zSelect.addEventListener('change', changeSelect)
